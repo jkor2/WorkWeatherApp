3ff4fa5df901289271852312c6ecc9c73b5f6fb9
Jake Korobellis <jkorobellis@gmail.com>
2023-06-28 20:00:17 -0400

    Making sure location is laoded before render

--- src/comp/dash.js
+++ src/comp/dash.js
@@ -1,19 +1,33 @@
+import React from "react";
+
 function Functionality() {
+  const [locationLoaded, setLocationLoaded] = React.useState(false);
+  //User location state
+  const [currLocation, setCurrLocation] = React.useState();
+
   //On success
   const successCallback = (position) => {
-    console.log(position);
+    setCurrLocation(position);
+    setLocationLoaded(true);
   };
   //On error
   const errorCallback = (error) => {
     console.log(error);
   };
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
   //Ask for current location
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
   return (
     <div className="App">
-      <h1>Connection Tested</h1>
+      {locationLoaded !== false ? (
+        <location>
+          <div>Your Current Location Is</div>
+          <div></div>
+        </location>
+      ) : (
+        <div>Finding your current location...</div>
+      )}
     </div>
   );
 }
