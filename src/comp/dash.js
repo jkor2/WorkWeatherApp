45d2803a5850e7a65711dd6979fb3a73e4adde3b
Jake Korobellis <jkorobellis@gmail.com>
2023-06-28 20:13:04 -0400

    Display current location

--- src/comp/dash.js
+++ src/comp/dash.js
@@ -4,27 +4,32 @@ function Functionality() {
   const [locationLoaded, setLocationLoaded] = React.useState(false);
   //User location state
   const [currLocation, setCurrLocation] = React.useState();
+  console.log(currLocation.coords.latitude);
+  React.useEffect(() => {
+    const successCallback = (position) => {
+      setCurrLocation(position);
+      setLocationLoaded(true);
+    };
 
-  //On success
-  const successCallback = (position) => {
-    setCurrLocation(position);
-    setLocationLoaded(true);
-  };
-  //On error
-  const errorCallback = (error) => {
-    console.log(error);
-  };
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    const errorCallback = (error) => {
+      console.log(error);
+    };
+
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }, []);
 
   //Ask for current location
 
   return (
     <div className="App">
       {locationLoaded !== false ? (
-        <location>
-          <div>Your Current Location Is</div>
-          <div></div>
-        </location>
+        <div>
+          <div className="title-location">Your Current Location Is</div>
+          <div className="coords">
+            <div>{currLocation.coords.latitude}</div>
+            <div>{currLocation.coords.longitude}</div>
+          </div>
+        </div>
       ) : (
         <div>Finding your current location...</div>
       )}
