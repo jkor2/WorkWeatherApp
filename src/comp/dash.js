4e8a2cb7969f7e0c7b53cbdfd68f4c1914d76a33
Jake Korobellis <jkorobellis@gmail.com>
2023-06-30 17:19:26 -0400

    edited search count for UI

--- src/comp/dash.js
+++ src/comp/dash.js
@@ -129,7 +129,7 @@ function Functionality() {
     // the string searched and for the second the results.
 
     fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${string}&count=10&language=en&format=json`
+      `https://geocoding-api.open-meteo.com/v1/search?name=${string}&count=4&language=en&format=json`
     )
       .then((res) => res.json())
       .then((data) => setSearchItem(data));
